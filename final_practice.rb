450461b9703c9bc8a2f2e1242085506f0e25c541
Jesse Thomas <jgthomas3@gmail.com>
2023-02-10 15:50:22 -0700

    Answer 5 final questions

--- final_practice.rb
+++ final_practice.rb
@@ -6,34 +6,85 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting 
+    "Hey there homie, what's good?"
+end
 
+
+# What is the return value of your method?
+    # The return value of my method is going to be a string that reads "Hey there homie, what's good?". The string 
+    # will print when the method is called on to print. 
+
+# How many arguments did you pass your method?
+    # There are no arguments passed nor expected in this defined method. 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def custom_greeting(name)
+    "Hey there #{name} my friend. What is good with this life and you?"
+end
 
+p custom_greeting("James")
+p custom_greeting("Josh")
+
+# What is the return value of your method?
+    # This method custom_greeting will print out the defined string in the code block with the interpolated variable replaced by the 
+    # specified arguments in the methods called on below the defined method above it. The return value will only print out 
+    # if the method is called on to print. 
+# How many arguments did you pass your method?
+    # I passed two different arguments through the method by calling on it two seperate times.
+# What data type was your argument(s)?
+    # Both arguments are Strings. 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def greet_person(first, middle, last)
+    "Hey now, I hear your full name is #{first} #{middle} #{last}, is that correct?"
+end
 
+p greet_person("Homer", "Jay", "Simpson")
+
+# What is the return value of your method?
+    # This method greet_person will print out the defined string in the code block with the 3 interpolated variables replaced by the 
+    # 3 specified arguments in the methods called on below the defined method above it. The return value will only print out 
+    # if the method is called on to print. The return value will read, "Hey now, I hear your full name is Homer Jay Simpson, is that correct?"
+# How many arguments did you pass your method?
+    # This method passed three arguments through the method by calling on it once. 
+# What data type was your argument(s)?
+    # All three arguments are Strings. 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square(int)
+    puts "Your value is #{Math.sqrt(int)}."
+end
+
+square(9)
+square(49)
+square(112)
 # What is the return value of your method?
+    # The square method defined above is going to return a string that reads, "Your value is ____". The blank space
+    # will be filled with an interpolated float that is based on the value of the argument given."
 # How many arguments did you pass your method?
+    # I passed 3 arguments through the method by calling on it 3 seperate times. 
 # What data type was your argument(s)?
+    # The data types in the arguments are integers and the data type in the return value is a float.
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(int1, item)
+    if int1 >= 4
+        puts "#{item} is stocked"
+    elsif int1 >= 1
+        puts "#{item} - runniing LOW"
+    else 
+        puts "#{item} - OUT of stock"
+    end
+end
+
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
